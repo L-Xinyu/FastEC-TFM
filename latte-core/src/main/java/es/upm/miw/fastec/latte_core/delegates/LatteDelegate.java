30921ee26baedad72ab699a9b162f6806924b629
XinyuLi <911410598@qq.com>
2020-05-12 15:49:19 +0200

    Add classes LatteDelegate and PermissionCheckerDelegate to determine permissions

---
+++ latte-core/src/main/java/es/upm/miw/fastec/latte_core/delegates/LatteDelegate.java
@@ -0,0 +1,4 @@
+package es.upm.miw.fastec.latte_core.delegates;
+
+public class LatteDelegate extends PermissionCheckerDelegate{
+}
---
+++ latte-core/src/main/java/es/upm/miw/fastec/latte_core/delegates/PermissionCheckerDelegate.java
@@ -0,0 +1,18 @@
+package es.upm.miw.fastec.latte_core.delegates;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PermissionCheckerDelegate extends BaseDelegate {
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}

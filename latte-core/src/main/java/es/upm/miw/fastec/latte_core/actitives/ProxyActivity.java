928340908171fe23a61e8bc7c2348ce2e2eea4a1
XinyuLi <911410598@qq.com>
2020-05-12 15:54:03 +0200

    Add methods to ProxyActivity

--- latte-core/src/main/java/es/upm/miw/fastec/latte_core/actitives/ProxyActivity.java
+++ latte-core/src/main/java/es/upm/miw/fastec/latte_core/actitives/ProxyActivity.java
@@ -1,6 +1,38 @@
 package es.upm.miw.fastec.latte_core.actitives;
 
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+
+import es.upm.miw.fastec.latte_core.R;
+import es.upm.miw.fastec.latte_core.delegates.LatteDelegate;
 import me.yokeyword.fragmentation.SupportActivity;
 
-public class ProxyActivity extends SupportActivity {
+public abstract class ProxyActivity extends SupportActivity {
+
+    public abstract LatteDelegate setRootDelegate();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void initContainer(@Nullable Bundle savedInstanceState){
+
+        final FrameLayout container = new FrameLayout(this);
+        container.setId(R.id.delegate_container);
+
+        setContentView(container);
+        if (savedInstanceState == null){
+            loadRootFragment(R.id.delegate_container,setRootDelegate());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
+        System.runFinalization();
+    }
 }

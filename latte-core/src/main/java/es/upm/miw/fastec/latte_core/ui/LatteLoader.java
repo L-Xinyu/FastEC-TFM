f09a2fe8e1de1b0ca36133b02d22abf540ba1dd6
XinyuLi <911410598@qq.com>
2020-05-15 16:15:15 +0200

    Create Loading framework and Add classes and style.xml

---
+++ latte-core/src/main/java/es/upm/miw/fastec/latte_core/ui/LatteLoader.java
@@ -0,0 +1,13 @@
+package es.upm.miw.fastec.latte_core.ui;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDialog;
+
+public class LatteLoader {
+
+    public static void showLoading(Context context,String type){
+
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+    }
+}
